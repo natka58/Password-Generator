fe5b8f9c5375deb18d5c892b5321a19945d9b7fc
Natalia Teaca <lilikoy@sbcglobal.net>
2020-06-04 01:48:25 -0700

    var

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,26 @@
-// Assignment code here
+
+document.getElementById('generate').onclick = function() {writePassword()};
+
+// var characters 
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var numbers = ["0123456789"];
+var punctuation = ["!@#$%^&*()_+~`|}{[]:;?><,./-="];
+
+// user character
+var characterTypes = [];
+var userLength;
+var userLowerCase;
+var userUpperCase;
+var userNumbers;
+var userPunctuation;
+var randomCharacter;
+var randomArray;
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
+// Prompt for user
+function writePassword () {
+  var userLength = (prompt ("How many characters do you want your password to be? Must be 8-128"));
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -13,5 +28,21 @@ function writePassword() {
 
 }
 
+// Password length 8 to 128 characters
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+
+// Set length of the password
+
+// Set to include character in password
+
+// Set to include lowercase, uppaercase. numeric, characters in password
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
